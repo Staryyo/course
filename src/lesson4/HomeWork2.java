b765dc256f9ed7042cd53a33d84dac771878f41e
Staryyo <kefir1002@mail.ru>
2023-03-01 09:23:12 +0300

    add Homework2

---
+++ src/lesson4/HomeWork2.java
@@ -0,0 +1,14 @@
+package lesson4;
+
+public class HomeWork2 {
+    public static void main(String... args) {
+        for (int i = 1; i < 11; i++) {
+            for (int f = 1; f < 11; f++) {
+                int c=f*i;
+                System.out.printf("%d ",c);
+            }
+            System.out.println(" ");
+
+        }
+    }
+}
